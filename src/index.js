06808ddef2c4ba630dd4295af83aa6372ddfba95
rubenevivancos <rubenevivancos@hotmail.com>
2024-06-03 02:51:20 -0500

    Conectando a backend de produccion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 
 import { store } from "./Redux/Store/store";
 import App from './App';
 
+
+axios.defaults.baseURL='https://pi-dogs-2-0-back-end.vercel.app';
 
 
 // Crea un root utilizando createRoot
